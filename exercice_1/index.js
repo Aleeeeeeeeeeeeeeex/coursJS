a9d8fd7122530f01a062012973ae48baf7c3c00b
Alexandre <carronalexandre77@gmail.com>
2022-06-30 15:38:37 +0200

    z

--- exercice_1/index.js
+++ exercice_1/index.js
@@ -1,4 +1,16 @@
 // Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+const rond_1 = document.querySelector(".rond_1");
+const rond_2 = document.querySelector(".rond_2");
+
+window.addEventListener("mousemove", (e) => {
+  rond_1.style.left = e.pageX + "px";
+  rond_1.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousemove", (e) => {
+  rond_2.style.left = e.pageX + "px";
+  rond_2.style.top = e.pageY + "px";
+});
 
 // Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
 
